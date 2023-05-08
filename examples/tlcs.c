256a6011d403fa1495de9b5b91155b5a86111da1
vincenzoiovino <vinciovino@gmail.com>
2023-05-08 15:55:14 +0200

    user interface for the demo

--- examples/tlcs.c
+++ examples/tlcs.c
@@ -4,6 +4,7 @@
 // Vincenzo Iovino, 2023, Aragon ZK Research
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 #include <stdlib.h>
 #include <mcl/bn_c384_256.h>
 #include "tlcs.h"
@@ -14,11 +15,24 @@
 #if _DEBUG_ ==1
 #include <time.h>
 #endif
-#define NUM_PARTIES 1
+#define NUM_PARTIES 3
 
 
+static time_t loe_genesis_time=1677685200;
+static unsigned int loe_period=3;
+inline static void set_loe_signature(G1 *Signature, const char *sigStr,size_t len) {
+mclBn_setETHserialization(1);
+ASSERT(!(mclBnG1_setStr(Signature, sigStr, len, MCLBN_IO_SERIALIZE_HEX_STR)));
+mclBn_setETHserialization(0);
+}
 
+static inline uint64_t current_time_to_current_round(time_t current){
+return (current-loe_genesis_time)/loe_period;
+}
 
+static inline uint64_t round_from_time(time_t current,unsigned int time_offset){
+return current_time_to_current_round(current)+3*time_offset;
+}
 
 int main()
 {
@@ -29,6 +43,17 @@ bool verified_proof[NUM_PARTIES];
 G1 Signature;
 CycGrpG GPK;
 CycGrpZp gsk;
+unsigned int time_offset;
+time_t current;
+
+
+
+
+//time_t current=genesis+1955231*3;
+ //   printf("%s", ctime(&current));
+//return 0;
+
+
 #if _DEBUG_ == 1
 clock_t begin,end;
 double time_spent;
@@ -37,7 +62,19 @@ ASSERT(!pairing_init());
 ASSERT(!group_init());
 generate_loe_publickey();
 
-round=1;
+//round=1954572;
+current=time(NULL);
+printf("Now it is %s",ctime(&current));
+//printf("Give me a round number in decimal format\n No check is done, so be careful to not add any unnecessary space or character.\n You can find the latest round here: https://api.drand.sh/dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493/public/latest\n");
+//scanf("%" SCNu64 "",&round);
+printf("\nInsert a number X. The protocol will be executed to compute a public key that will be invertible 3*X seconds in the future since now.\nExample: if you insert 100 the protocol will output a public key that will be invertible in 5 minutes.\nNo check is done, so be careful to not add any unnecessary space or character.\n");
+scanf("%u",&time_offset);
+//You can find the latest round here: https://api.drand.sh/dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493/public/latest\n");
+round=round_from_time(current,time_offset);
+#if _DEBUG_ == 1
+
+printf("Executing the protocol for round %" SCNu64 "\n",round);
+#endif
 for (i=0;i<NUM_PARTIES;i++){
 
 #if _DEBUG_ == 1
@@ -49,19 +86,9 @@ ASSERT(!Prover(&P[i],round));
 #if _DEBUG_ == 1
 end = clock();
  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-printf("time spent by prover on a single party: %fs\n",time_spent);
+printf("time spent by party %d in computing his public key and proof: %fs\n",i,time_spent);
 #endif
 
-#if PK_SIMULATED == 1 // in this case we are generating the signature by ourself because we simulated the public key and we know the corresponding secret key
-generate_loe_signature(&Signature,round);
-#else // in this case we must set signature Signature to a real LOE's signature
-{
-const  char *sigStr="9544ddce2fdbe8688d6f5b4f98eed5d63eee3902e7e162050ac0f45905a55657714880adabe3c3096b92767d886567d0"; // this is the signature for round 1 of the unchained chain
-mclBn_setETHserialization(1);
-ASSERT(!(mclBnG1_setStr(&Signature, sigStr, strlen(sigStr), MCLBN_IO_SERIALIZE_HEX_STR)));
-mclBn_setETHserialization(0);
-}
-#endif
 
 #if _DEBUG_ == 1
 begin = clock();
@@ -70,7 +97,7 @@ ASSERT(!(ret=Verifier(&P[i].PK,&P[i].pi,round)));
 #if _DEBUG_ == 1
 end = clock();
  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-printf("time spent by verifier on a single party: %fs\n",time_spent);
+printf("time spent by verifier on verifying proof of party %d: %fs\n",i,time_spent);
 #endif
 if (ret==0) verified_proof[i]=true;
 else verified_proof[i]=false;
@@ -79,10 +106,26 @@ AggregatePublicKeys(&GPK,P,NUM_PARTIES,verified_proof);
 }
 // uncomment as sanity check tp test a failed inversion (supposes inversion error for party i=3, so set NUM_PARTIES>=4)
 //CycGrpG_add(&P[3].pi.C[0][0].PK,&P[3].pi.C[0][0].PK,&P[3].pi.C[0][0].PK);
+#if PK_SIMULATED == 1 // in this case we are generating the signature by ourself because we simulated the public key and we know the corresponding secret key
+generate_loe_signature(&Signature,round);
+#else // in this case we must set signature Signature to a real LOE's signature
+//const  char *sigStr="9544ddce2fdbe8688d6f5b4f98eed5d63eee3902e7e162050ac0f45905a55657714880adabe3c3096b92767d886567d0"; // this is the signature for round 1 of the unchained chain
+//const char *sigStr="adda388d25a165bfb0efd8dba2f8ddab45ca7cbfdb64cbc7147aada71bf49a5239896c0608beed0ddbe24718d2d8b358"; // signature for round 1954572
+printf("\nGive me a signature in hexadecimal format for the round %" SCNu64 "\n You can find it here: https://api.drand.sh/dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493/public/",round);
+printf("%" SCNu64 "/\n",round);
+{
+time_t t=current+loe_period*time_offset;
+printf("This website will be available only at time %s",ctime(&t));
+}
+char sigStr[96+1]; // 96 is the length in hexadecimal of G1 points serialized in compressed form as published by LOE
+scanf("%s",sigStr);
+set_loe_signature(&Signature,sigStr,strlen(sigStr));
+#endif
 {
 CycGrpG Recovered_PK;
 //Invert(&inverted_sk,&P[0].PK,&Signature, &P[0].pi);
 #if _DEBUG_ == 1
+printf("\n");
 begin = clock();
 #endif
 i=InvertAggregate(&gsk,P,NUM_PARTIES,&Signature,verified_proof);
@@ -100,7 +143,7 @@ return 1;
 }
 #if _DEBUG_ == 1
 else 
-printf("general secret key successfully inverted\n");
+printf("general secret key for round %lu successfully inverted\n",round);
 #endif
 }
 err();
