28d871a1bd8b86839b263df1ab916b5abf492793
vincenzoiovino <vinciovino@gmail.com>
2023-06-07 15:28:29 +0200

    minor

--- bin/prover4blockchain.sh
+++ bin/prover4blockchain.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm tmp
-echo -ne "714\n1\n" > tmp
-rm tmpproof
-./bin/prover4blockchain tmpproof <tmp
-cat tmpproof
-rm tmpproof
-rm tmp
+  echo -ne "714\n"
+echo -ne $1 >>tmp
+echo -ne "\n" >> tmp
+  rm tmpproof. / bin / prover4blockchain tmpproof < tmp
+  cat tmpproof rm tmpproof rm tmp
