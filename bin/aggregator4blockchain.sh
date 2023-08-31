33737e2c2379b739ea86e5dd53487b6b1690b5d8
vincenzoiovino <vinciovino@gmail.com>
2023-08-31 10:00:05 +0200

    unused files

--- bin/aggregator4blockchain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-  echo -ne "714\n" >tmp
-cat > tmpproof
-  ./bin/aggregator4blockchain tmpproof tmpaggregatedpk $@ < tmp
-  cat tmpaggregatedpk
-  rm -f tmpproof
-  rm -f tmpaggregatedpk
-  rm -f tmp
-
--- bin/prover4blockchain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-  echo -ne "714\n" >tmp
-  echo -ne $1 >>tmp
-  echo -ne "\n" >> tmp
-  ./bin/prover4blockchain tmpproof < tmp
-  cat tmpproof 
-  rm -f tmpproof
-  rm -f tmp
